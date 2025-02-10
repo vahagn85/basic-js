3ae5de00314739ac284d57c1f006ad202bfc8c3e
vahagn85 <v.ulikhanyan@yandex.ru>
2025-02-10 22:39:36 +0400

    feat: implement getEmailDomain and commonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let obj = {};
+  for (let s of s1) {
+    obj[s] = ++obj[s] || 1;
+  }
+  let count = 0;
+  for (let o of s2) {
+    if (o in obj && obj[o]) {
+      count++;
+      --obj[o];
+    }
+  }
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
